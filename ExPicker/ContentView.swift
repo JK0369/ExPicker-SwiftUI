bf851846d723a335161726fab0d7ac249984726f
jake.k <jake.k@hpcnt.com>
2022-08-09 23:37:24 +0900

    Implement

--- ExPicker/ContentView.swift
+++ ExPicker/ContentView.swift
@@ -7,15 +7,70 @@
 
 import SwiftUI
 
+enum Flavor: String, CaseIterable, Identifiable {
+  case chocolate, vanilla, strawberry
+  var id: Self { self }
+}
+
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+  @State private var selectedFlavor: Flavor = .chocolate
+  
+  var body: some View {
+    Picker("Flavor", selection: $selectedFlavor) {
+      ForEach(Flavor.allCases) { Text($0.rawValue.capitalized) }
     }
+    .pickerStyle(.automatic)
+
+    
+//    List {
+//      Picker("Flavor", selection: $selectedFlavor) {
+//        ForEach(Flavor.allCases) { Text($0.rawValue.capitalized) }
+//      }
+//      .pickerStyle(.inline)
+//      Text("Selected flavor: \(selectedFlavor.rawValue)")
+//    }
+    
+    
+//    Picker("Flavor", selection: $selectedFlavor) {
+//      ForEach(Flavor.allCases) { Text($0.rawValue.capitalized) }
+//    }
+//    .pickerStyle(.wheel)
+
+    
+//    List {
+//      Picker("Flavor", selection: $selectedFlavor) {
+//        ForEach(Flavor.allCases) { Text($0.rawValue.capitalized) }
+//      }
+//    }
+
+    
+//    Picker("Flavor", selection: $selectedFlavor) {
+//      ForEach(Flavor.allCases) { Text($0.rawValue.capitalized) }
+//    }
+//    .pickerStyle(.segmented)
+
+    
+//    Picker("Flavor", selection: $selectedFlavor) {
+//      ForEach(Flavor.allCases) { Text($0.rawValue.capitalized) }
+//    }
+//    .pickerStyle(.menu)
+
+    
+//    Picker("Flavor", selection: $selectedFlavor) {
+//      ForEach(Flavor.allCases) { Text($0.rawValue.capitalized) }
+//    }
+//    .pickerStyle(.radioGroup)
+
+    
+//    Picker("Flavor", selection: $selectedFlavor) {
+//      ForEach(Flavor.allCases) { Text($0.rawValue.capitalized) }
+//    }
+//    .pickerStyle(.wheel)
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
