55eae2e9aa00dbaba3debb7e1f54331bfbdf4742
jake.k <jake.k@hpcnt.com>
2022-08-09 22:28:06 +0900

    Initial Commit

---
+++ ExPicker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ExPicker
+//
+//  Created by Jake.K on 2022/08/09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ExPicker/ExPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExPickerApp.swift
+//  ExPicker
+//
+//  Created by Jake.K on 2022/08/09.
+//
+
+import SwiftUI
+
+@main
+struct ExPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
